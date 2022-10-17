c5c2366176756a2477836815da3fb35f023c8f8a
eddieh03 <edonehaziri@gmail.com>
2022-10-17 14:05:18 +0200

    fix

--- ReflectBlog/Program.cs
+++ ReflectBlog/Program.cs
@@ -66,11 +66,11 @@ app.MapControllers();
 
 app.UseEndpoints(endpoints =>
 {
-    endpoints.MapGet("/", async context =>
-    {
-        context.Response.Redirect("/swagger", permanent: false);
-        await Task.CompletedTask;
-    });
+    //endpoints.MapGet("/", async context =>
+    //{
+    //    context.Response.Redirect("/swagger", permanent: false);
+    //    await Task.CompletedTask;
+    //});
     endpoints.MapControllers();
     //endpoints.MapRazorPages();
 });
